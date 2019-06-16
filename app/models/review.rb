53da36ef10dde2dc82abb1e26da90cbde3b32f8b
Britta Jorgenson <bljorgen@gmail.com>
2019-06-16 00:30:44 +0000

    update validation for review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
 
-  validates :rating, presence: true
+  validates :product, presence: true
+  validates :user, presence: true
+  validates :rating, presence: true, numericality: {only_integer: true, greater_than: 0, less_than: 6}
 end
