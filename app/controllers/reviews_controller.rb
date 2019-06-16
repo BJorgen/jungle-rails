f1c66f73b9203f31135afe0263592eb68e0205c5
Britta Jorgenson <bljorgen@gmail.com>
2019-06-16 23:47:16 +0000

    update reviews controller to create a new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,14 @@
 class ReviewsController < ApplicationController
   def create
-    raise "Yay, I'm here!"
+    @product = Product.find(params['product_id'])
+    rating = params['review']['rating']
+    description = params['review']['description']
+    @review = @product.reviews.create(rating: rating, description: description)
+    @review.user = current_user
+    if @review.save
+      redirect_to '/'
+    else
+      redirect_to '/products/:product_id'
+    end
   end
 end
