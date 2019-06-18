b29e8dd35a2e628feeab500292b907b3b58fd7a5
Britta Jorgenson <bljorgen@gmail.com>
2019-06-18 17:13:51 +0000

    complete specs for testing of product validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,12 +2,44 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    it 'should be created with all fields defined' do
+
+    subject {
       @category = Category.new
       @category.save
-      @product = @category.products.create(name: "ball", price: 5, quantity: 15)
-      expect(@product.id).to be_present
+      described_class.new(
+        name: "ball", 
+        price_cents: 5, 
+        quantity: 15, 
+        category: @category)
+    }
+
+    it 'should be created with all fields defined' do
+      expect(subject).to be_valid
     end
-  # validation tests/examples here
+
+    it 'should not be created without a name defined' do
+      subject.name = nil
+      subject.save
+      expect(subject.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'should not be created without a price defined' do
+      subject.price_cents = nil
+      subject.save
+      expect(subject.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'should not be created without a quantity defined' do
+      subject.quantity = nil
+      subject.save
+      expect(subject.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'should be not be created without a category defined' do
+      subject.category = nil
+      subject.save
+      expect(subject.errors.full_messages).to include("Category can't be blank")
+    end
+
   end
 end
